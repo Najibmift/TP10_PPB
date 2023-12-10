ecd7a44a494e7eecf13943d379124fdabbe8a4f0
Najibmift <najibmiftahul123@gmail.com>
2023-12-11 00:15:45 +0700

    mengubah tampilan body

--- my_first_flutter/lib/screens/test_firestore.dart
+++ my_first_flutter/lib/screens/test_firestore.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class TestFirestore extends StatefulWidget {
-  const TestFirestore({super.key});
+  const TestFirestore({Key? key}) : super(key: key);
 
   @override
   State<TestFirestore> createState() => _TestFirestoreState();
@@ -10,23 +10,24 @@ class TestFirestore extends StatefulWidget {
 
 class _TestFirestoreState extends State<TestFirestore> {
   var db = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.teal,
         title: Text('Home Firestore'),
+        backgroundColor: Colors.teal,
       ),
       body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
         stream: db.collection('users').snapshots(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
+            return Center(
               child: CircularProgressIndicator(),
             );
           }
           if (snapshot.hasError) {
-            return const Center(
+            return Center(
               child: Text("Error"),
             );
           }
@@ -36,9 +37,23 @@ class _TestFirestoreState extends State<TestFirestore> {
           return ListView.builder(
             itemCount: _data.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(_data[index].data()["first"].toString()),
-                subtitle: Text(_data[index].data()["last"].toString()),
+              return Card(
+                elevation: 3,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: ListTile(
+                  title: Text(
+                    _data[index].data()!["first"].toString(),
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(_data[index].data()!["last"].toString()),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      // Hapus data berdasarkan ID
+                      db.collection("users").doc(_data[index].id).delete();
+                    },
+                  ),
+                ),
               );
             },
           );
@@ -50,14 +65,16 @@ class _TestFirestoreState extends State<TestFirestore> {
         onPressed: () {
           // Create a new user with a first and last name
           final user = <String, dynamic>{
-            "first": "Ada",
-            "last": "Lovelace",
-            "born": 1825,
+            "first": "John Doe",
+            "last": "Savage",
+            "born": 2000,
           };
 
-// Add a new document with a generated ID
-          db.collection("users").add(user).then((DocumentReference doc) =>
-              print('DocumentSnapshot added with ID: ${doc.id}'));
+          // Tambahkan dokumen baru dengan ID yang dibuat otomatis
+          db.collection("users").add(user).then(
+                (DocumentReference doc) =>
+                    print('DocumentSnapshot added with ID: ${doc.id}'),
+              );
         },
         child: Icon(
           Icons.add,
