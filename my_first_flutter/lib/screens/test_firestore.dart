ad108e008ede9a11b5bc7e74577b63e3ff276a72
Zinc4 <65228679+Zinc4@users.noreply.github.com>
2023-12-10 22:40:02 +0700

    nambah text Home

--- my_first_flutter/lib/screens/test_firestore.dart
+++ my_first_flutter/lib/screens/test_firestore.dart
@@ -13,7 +13,9 @@ class _TestFirestoreState extends State<TestFirestore> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('HOME'),
+      ),
       body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
         stream: db.collection('users').snapshots(),
         builder: (context, snapshot) {
